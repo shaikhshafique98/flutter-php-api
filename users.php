09afd622db8f1fc3fc29a81618a6ae2b32e62ad8
shaikhshafique98 <shaikhshafique98@gmail.com>
2025-04-21 15:45:40 +0530

    Update users.php

--- users.php
+++ users.php
@@ -1,9 +1,6 @@
 
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: *");
-header("Content-Type: application/json");
+
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
